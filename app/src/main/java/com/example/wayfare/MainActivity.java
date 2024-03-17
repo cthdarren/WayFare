e960ad29a97ee04b472652fdd7a38f3b996da06d
Gabryel <gabryelsoh@gmail.com>
2024-03-18 03:15:16 +0800

    Added fragments manager, check previous version to resolvesetOnItemReselectedListener

--- app/src/main/java/com/example/wayfare/MainActivity.java
+++ app/src/main/java/com/example/wayfare/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.wayfare;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -15,7 +18,26 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot()); // saves time, no need to write findbyidelement each time
 
-        binding.
+        replaceFragment(new ExploreFragment());
+        binding.bottomNavigationView.setBackground(null);
+
+        binding.bottomNavigationView.setOnItemReselectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.explore -> replaceFragment(new ExploreFragment());
+                case R.id.bookmarks -> replaceFragment(new BookmarksFragment());
+                case R.id.tours -> replaceFragment(new ToursFragment());
+                case R.id.inbox -> replaceFragment(new InboxFragment());
+                case R.id.account -> replaceFragment(new AccountFragment());
+            }
+        });
     }
 
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.frame_layout, fragment)
+                .setReorderingAllowed(true)
+                .addToBackStack("name") // Name can be null
+                .commit();
+    }
 }
