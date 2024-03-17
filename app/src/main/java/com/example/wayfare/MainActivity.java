f342b2687a8cdcd32ed7846f2f03069412130025
Gabryel <gabryelsoh@gmail.com>
2024-03-18 02:52:36 +0800

    added version 1 of new navbar into activity_main, added fragment files into com.example.wayfare

--- app/src/main/java/com/example/wayfare/MainActivity.java
+++ app/src/main/java/com/example/wayfare/MainActivity.java
@@ -2,6 +2,20 @@ package com.example.wayfare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Bundle;
+
+import com.example.wayfare.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot()); // saves time, no need to write findbyidelement each time
+
+        binding.
+    }
 
 }
