3578d97dc250e3a2ba5605bfba92c2da6f716fbf
haresh02 <haresh.jayant@gmail.com>
2024-03-21 23:32:58 +0800

    new landing page for login and register

--- app/src/main/java/com/example/wayfare/SignInActivity.java
+++ app/src/main/java/com/example/wayfare/SignInActivity.java
@@ -1,6 +1,9 @@
 package com.example.wayfare;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +11,76 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+
 public class SignInActivity extends AppCompatActivity {
+
+    Button sign_in_button;
+    EditText et_usernamelog, et_passwordlog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_sign_in);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        et_usernamelog = findViewById(R.id.usernamelog);
+        et_passwordlog = findViewById(R.id.passwordlog);
+        sign_in_button = findViewById(R.id.sign_in_button);
+
+        sign_in_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                authenticateUser();
+            }
         });
+
     }
+
+    public void authenticateUser(){
+
+        RequestQueue queue = Volley.newRequestQueue(SignInActivity.this);
+        // URL to POST
+        String url = "http://143.198.223.202/api/auth/login";
+
+        // Set Parameters:
+        HashMap<String, String> params = new HashMap<String, String>();
+        params.put("usernamelog", et_usernamelog.getText().toString());
+        params.put("passwordlog", et_passwordlog.getText().toString());
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, new JSONObject(params), new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject jsonObject) {
+                try {
+                    String firstName = (String) jsonObject.get("firstName");
+                    String lastName = (String) jsonObject.get("lastName");
+                    String email = (String) jsonObject.get("email");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    System.out.println(e.getMessage());
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+
+            }
+        });
+
+    }
+
+
+
+
 }
