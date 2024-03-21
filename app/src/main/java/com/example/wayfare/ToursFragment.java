2f48e50fc979df87b53cb47dc0f20261287ff74b
Gabryel <gabryelsoh@gmail.com>
2024-03-22 07:20:39 +0800

    using strings.xml and setUpTourListModels in ToursFragment.java to get test db of images and titles for tour listings on Tours fragment

--- app/src/main/java/com/example/wayfare/ToursFragment.java
+++ app/src/main/java/com/example/wayfare/ToursFragment.java
@@ -14,6 +14,9 @@ import java.util.ArrayList;
 
 public class ToursFragment extends Fragment {
     private RecyclerView recyclerView;
+    ArrayList<TourListModel> tourListModels = new ArrayList<>();
+    // holding all models to send to adapter later on
+    int[] tourListingImages = {R.drawable.guide1,R.drawable.guide2,R.drawable.guide3,R.drawable.guide4,R.drawable.guide5};
     public ToursFragment(){}
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -21,31 +24,26 @@ public class ToursFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_tours, container, false);
         recyclerView = view.findViewById(R.id.myRecyclerView);
-        //setUpTourListModels();
-        ArrayList<TourListModel> items = new ArrayList<TourListModel>();
 
-        items.add(new TourListModel("Chinatown tour", R.drawable.guide1));
-        items.add(new TourListModel("Little India tour", R.drawable.guide2));
-        items.add(new TourListModel("Kampong Glam tour", R.drawable.guide3));
-        items.add(new TourListModel("Lau Pa Sat tour", R.drawable.guide4));
-        items.add(new TourListModel("SUTD tour", R.drawable.guide5));
+        //ArrayList<TourListModel> tourListModels = new ArrayList<TourListModel>();
+        setUpTourListModels();
+//        items.add(new TourListModel("Chinatown tour", R.drawable.guide1));
+//        items.add(new TourListModel("Little India tour", R.drawable.guide2));
+//        items.add(new TourListModel("Kampong Glam tour", R.drawable.guide3));
+//        items.add(new TourListModel("Lau Pa Sat tour", R.drawable.guide4));
+//        items.add(new TourListModel("SUTD tour", R.drawable.guide5));
 
 
         recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
-        recyclerView.setAdapter(new tourListing_RecyclerViewAdapter(view.getContext(), items));
+        recyclerView.setAdapter(new tourListing_RecyclerViewAdapter(view.getContext(), tourListModels));
         return view;
     }
-
-    ArrayList<TourListModel> tourListModels = new ArrayList<>();
-    // holding all models to send to adapter later on
-
-    int[] tourListingImages = {R.drawable.ic_launcher_background};
-
 
     private void setUpTourListModels(){
         // not using for first implementation of feature
         String[] tourListingTitles = getResources().getStringArray(R.array.tours_fragment_list_titles);
         for (int i = 0; i < tourListingTitles.length; i++){
+            // note that in this implementation, the length of the array must be the same as the number of images
             tourListModels.add(new TourListModel(tourListingTitles[i], tourListingImages[i]));
         }
     }
