78fee33bfc6020b81f9a5faa6a801ced7d0020c1
Gabryel <gabryelsoh@gmail.com>
2024-03-22 06:20:53 +0800

    added listing recyclerview function for tours, need to fix searchbar element not showing

---
+++ app/src/main/java/com/example/wayfare/TourListModel.java
@@ -0,0 +1,18 @@
+package com.example.wayfare;
+// we are creating variables thats gonna hold all the data that represents one of our items
+public class TourListModel {
+    public TourListModel(String tourListingTitle,  int tourListingImage) {
+        this.tourListingTitle = tourListingTitle;
+        this.tourListingImage = tourListingImage;
+    }
+
+    String tourListingTitle;
+    int tourListingImage;
+
+    public String getTourListingTitle() {
+        return tourListingTitle;
+    }
+    public int getTourListingImage() {
+        return tourListingImage;
+    }
+}
--- app/src/main/java/com/example/wayfare/ToursFragment.java
+++ app/src/main/java/com/example/wayfare/ToursFragment.java
@@ -3,30 +3,50 @@ package com.example.wayfare;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
 public class ToursFragment extends Fragment {
+    private RecyclerView recyclerView;
     public ToursFragment(){}
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tours, container, false);
+        View view = inflater.inflate(R.layout.fragment_tours, container, false);
+        recyclerView = view.findViewById(R.id.myRecyclerView);
+        //setUpTourListModels();
+        ArrayList<TourListModel> items = new ArrayList<TourListModel>();
+
+        items.add(new TourListModel("Chinatown tour", R.drawable.guide1));
+        items.add(new TourListModel("Little India tour", R.drawable.guide2));
+        items.add(new TourListModel("Kampong Glam tour", R.drawable.guide3));
+        items.add(new TourListModel("Lau Pa Sat tour", R.drawable.guide4));
+        items.add(new TourListModel("SUTD tour", R.drawable.guide5));
+
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+        recyclerView.setAdapter(new tourListing_RecyclerViewAdapter(view.getContext(), items));
+        return view;
     }
 
-//    @Override
-//    public void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.fragment_tours);
-//
-//        // Get the intent, verify the action, and get the query.
-//        Intent intent = getIntent();
-//        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
-//            String query = intent.getStringExtra(SearchManager.QUERY);
-//            doMySearch(query);
-//        }
-//    }
+    ArrayList<TourListModel> tourListModels = new ArrayList<>();
+    // holding all models to send to adapter later on
+
+    int[] tourListingImages = {R.drawable.ic_launcher_background};
+
+
+    private void setUpTourListModels(){
+        // not using for first implementation of feature
+        String[] tourListingTitles = getResources().getStringArray(R.array.tours_fragment_list_titles);
+        for (int i = 0; i < tourListingTitles.length; i++){
+            tourListModels.add(new TourListModel(tourListingTitles[i], tourListingImages[i]));
+        }
+    }
 }
