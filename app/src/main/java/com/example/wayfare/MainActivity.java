faa98e198c31aebb4cf601f941377f094b8de7fb
Gabryel <gabryelsoh@gmail.com>
2024-03-22 15:31:35 +0800

    swapped constraintLayout with coordinatorLayout and added layout_anchor to link searchView and search_bar for default searchview expand animation

--- app/src/main/java/com/example/wayfare/MainActivity.java
+++ app/src/main/java/com/example/wayfare/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.SearchView;
 
 import com.example.wayfare.databinding.ActivityMainBinding;
 
--- app/src/main/java/com/example/wayfare/ToursFragment.java
+++ app/src/main/java/com/example/wayfare/ToursFragment.java
@@ -2,6 +2,9 @@ package com.example.wayfare;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -9,6 +12,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 
 import java.util.ArrayList;
 
@@ -38,11 +42,13 @@ public class ToursFragment extends Fragment {
 
         recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
         recyclerView.setAdapter(new tourListing_RecyclerViewAdapter(view.getContext(), tourListModels));
-
-
-        // search bar
-
         return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //((SearchView) view.findViewById(R.id.searchView)).setUpWithSearchBar(R.id.search_bar);
     }
 
     private void setUpTourListModels(){
