cff2d50291dd8146499a255bb0001e8e9bacea8d
gangeshk14 <gangeshk14@gmail.com>
2024-03-24 00:37:13 +0800

    directory structure changed,swipe functionality,error with sound

---
+++ app/src/main/java/com/example/wayfare/Activity/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.wayfare.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.wayfare.Fragment.AccountFragment;
+import com.example.wayfare.Fragment.BookmarksFragment;
+import com.example.wayfare.Fragment.ExploreFragment;
+import com.example.wayfare.Fragment.InboxFragment;
+import com.example.wayfare.R;
+import com.example.wayfare.Fragment.ToursFragment;
+import com.example.wayfare.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = (View) binding.getRoot();
+        setContentView(view);
+        View decorView = getWindow().getDecorView();
+        replaceFragment(new ExploreFragment());
+        //binding.bottomNavigationView.setBackground(null);
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.explore){
+                replaceFragment(new ExploreFragment());
+            } else if (item.getItemId() == R.id.bookmarks) {
+                replaceFragment(new BookmarksFragment());
+            } else if (item.getItemId() == R.id.tours) {
+                replaceFragment(new ToursFragment());
+            } else if (item.getItemId() == R.id.inbox) {
+                replaceFragment(new InboxFragment());
+            } else if (item.getItemId() == R.id.account) {
+                replaceFragment(new AccountFragment());
+            }
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.flFragment, fragment)
+                .setReorderingAllowed(true)
+                .addToBackStack("name") // Name can be null
+                .commit();
+    }
+
+
+}
