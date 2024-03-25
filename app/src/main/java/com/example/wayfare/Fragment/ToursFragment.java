722bfc05da13dfe70f9d385468d6fb7491ac71e4
Gabryel <gabryelsoh@gmail.com>
2024-03-25 12:24:12 +0800

    commented out Theme.Wayfare for android:tint to remove black tint over images. Fixed layout issues with tourlisting cards

--- app/src/main/java/com/example/wayfare/Fragment/ToursFragment.java
+++ app/src/main/java/com/example/wayfare/Fragment/ToursFragment.java
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,7 +25,7 @@ public class ToursFragment extends Fragment {
     private RecyclerView recyclerView;
     ArrayList<TourListModel> tourListModels = new ArrayList<>();
     // holding all models to send to adapter later on
-    int[] tourListingImages = {R.drawable.guide1,R.drawable.guide2,R.drawable.guide3,R.drawable.guide4,R.drawable.guide5};
+    int[] tourListingImages = {R.drawable.guide1, R.drawable.guide2, R.drawable.guide3, R.drawable.guide4, R.drawable.guide5};
     public ToursFragment(){}
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -34,14 +35,14 @@ public class ToursFragment extends Fragment {
         recyclerView = view.findViewById(R.id.myRecyclerView);
 
         //ArrayList<TourListModel> tourListModels = new ArrayList<TourListModel>();
-        setUpTourListModels();
+        //setUpTourListModels();
 
-        // use this to test custom models
-//        items.add(new TourListModel("Chinatown tour", R.drawable.guide1));
-//        items.add(new TourListModel("Little India tour", R.drawable.guide2));
-//        items.add(new TourListModel("Kampong Glam tour", R.drawable.guide3));
-//        items.add(new TourListModel("Lau Pa Sat tour", R.drawable.guide4));
-//        items.add(new TourListModel("SUTD tour", R.drawable.guide5));
+//         use this to test custom models
+        tourListModels.add(new TourListModel("Chinatown tour", R.drawable.guide1));
+        tourListModels.add(new TourListModel("Little India tour", R.drawable.guide2));
+        tourListModels.add(new TourListModel("Kampong Glam tour", R.drawable.guide3));
+        tourListModels.add(new TourListModel("Lau Pa Sat tour", R.drawable.guide4));
+        tourListModels.add(new TourListModel("SUTD tour", R.drawable.guide5));
 
 
         recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
