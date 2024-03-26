fd7c7eec3cceb2d6c210cf6330ff2a7c239c64bd
FooChuanTian <foochuantian@gmail.com>
2024-03-26 20:43:36 +0800

    Adding New Tour fragment and Tour Listing Page In Progress

--- app/src/main/java/com/example/wayfare/Fragment/TourListingFragment.java
+++ app/src/main/java/com/example/wayfare/Fragment/TourListingFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,16 +13,22 @@ import androidx.fragment.app.Fragment;
 import com.example.wayfare.R;
 
 public class TourListingFragment extends Fragment {
+
+    TextView listingTitle;
+    TextView listingDescription;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_tours, container, false);
-        //
+
         return view;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        listingDescription =(TextView) getView().findViewById(R.id.listingDescription);
+        listingTitle = (TextView) getView().findViewById(R.id.listingTitle);
     }
 }
