e88913c889fba4a50c9dee5fa87329657226d95c
FooChuanTian <foochuantian@gmail.com>
2024-03-26 20:43:36 +0800

    Adding New Tour fragment and Tour Listing Page In Progress

--- app/src/main/java/com/example/wayfare/Fragment/TourListingFragment.java
+++ app/src/main/java/com/example/wayfare/Fragment/TourListingFragment.java
@@ -4,11 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-<<<<<<< HEAD
+
 import android.widget.TextView;
 
-=======
->>>>>>> 4b5dfec (Added skeleton of Tour Listing Page and for Adding New Tour Listings)
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -17,32 +16,27 @@ import androidx.fragment.app.Fragment;
 import com.example.wayfare.R;
 
 public class TourListingFragment extends Fragment {
-<<<<<<< HEAD
+
 
     TextView listingTitle;
     TextView listingDescription;
 
-=======
->>>>>>> 4b5dfec (Added skeleton of Tour Listing Page and for Adding New Tour Listings)
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_tours, container, false);
-<<<<<<< HEAD
 
-=======
->>>>>>> 4b5dfec (Added skeleton of Tour Listing Page and for Adding New Tour Listings)
-        //
+
+
         return view;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-<<<<<<< HEAD
+
         listingDescription =(TextView) getView().findViewById(R.id.listingDescription);
         listingTitle = (TextView) getView().findViewById(R.id.listingTitle);
-=======
->>>>>>> 4b5dfec (Added skeleton of Tour Listing Page and for Adding New Tour Listings)
+
     }
 }
