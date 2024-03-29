52d8bea7170d92d4ad35269a6d4a37c83f8f773b
Gabryel <gabryelsoh@gmail.com>
2024-03-29 21:50:26 +0800

    navigate back using toolbar added to confirm and book page

--- app/src/main/java/com/example/wayfare/Activity/ConfirmBooking.java
+++ app/src/main/java/com/example/wayfare/Activity/ConfirmBooking.java
@@ -1,10 +1,14 @@
 package com.example.wayfare.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toolbar;
 
 import com.example.wayfare.R;
+import com.google.android.material.appbar.MaterialToolbar;
 
 public class ConfirmBooking extends AppCompatActivity {
 
@@ -12,5 +16,20 @@ public class ConfirmBooking extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirm_booking);
+        MaterialToolbar toolbar = findViewById(R.id.materialToolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("");
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle clicks on the Home/Up button in the action bar
+        if (item.getItemId() == android.R.id.home) {
+            // Finish the activity
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
