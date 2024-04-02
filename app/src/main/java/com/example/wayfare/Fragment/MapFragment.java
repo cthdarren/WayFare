b57b938ba151477f2901bf78a8c5aef788cab1d8
FooChuanTian <foochuantian@gmail.com>
2024-04-02 20:46:49 +0800

    MapView in progress

--- app/src/main/java/com/example/wayfare/Fragment/MapFragment.java
+++ app/src/main/java/com/example/wayfare/Fragment/MapFragment.java
@@ -7,11 +7,57 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import com.example.wayfare.Utils.AuthHelper;
+import com.example.wayfare.Utils.AuthService;
+import com.example.wayfare.Utils.Helper;
+import com.example.wayfare.ViewModel.UserViewModel;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.os.Handler;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.widget.Toast;
+
+import com.example.wayfare.Adapters.ShortsAdapter;
+import com.example.wayfare.Models.ShortsObject;
+import com.example.wayfare.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
-public class MapFragment extends Fragment {
-    private SupportMapFragment supportMapFragment;
+public class MapFragment extends Fragment implements OnMapReadyCallback {
+    private MapView mapView;
+    public MapFragment(){}
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_map, container, false);
+        mapView = view.findViewById(R.id.map);
+        return view;
+
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        mapView.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+    }
 }
