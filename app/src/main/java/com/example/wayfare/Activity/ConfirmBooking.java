dfa468f20e44d5758eff2b9d0b9efb4db8b1cbaa
Gabryel <gabryelsoh@gmail.com>
2024-04-03 19:50:11 +0800

    converted price to double LOL

--- app/src/main/java/com/example/wayfare/Activity/ConfirmBooking.java
+++ app/src/main/java/com/example/wayfare/Activity/ConfirmBooking.java
@@ -158,7 +158,7 @@ public class ConfirmBooking extends AppCompatActivity {
     public void createBooking(){
         Log.d("BUTTONS", "User tapped the confirm button");
         //TourListing listing, String userId, TimeRange bookingDuration, Date dateBooked, Double bookingPrice, int pax, String remarks
-        String json = String.format("{\"title\":\"%s\", \"userId\":\"%s\", \"timing\":\"%s\", \"dateBooked\":\"%s\", \"price\":\"%s\", \"pax\":\"%s\", \"remarks\":\"%s\"}", title, listingId, timeSlot, date, price, pax, remark);
+        String json = String.format("{\"title\":\"%s\", \"userId\":\"%s\", \"timing\":\"%s\", \"dateBooked\":\"%s\", \"price\":\"%s\", \"pax\":\"%s\", \"remarks\":\"%s\"}", title, listingId, timeSlot, date, Double.valueOf(price), pax, remark);
         RequestBody body = RequestBody.create(MediaType.parse("application/json"), json);
         new AuthService(this).getResponse("/booking/create/" + listingId, true, Helper.RequestType.REQ_POST, body, new AuthService.ResponseListener() {
             @Override
