f172927d009baa48d8448ea2bbbadc09930e7794
cthdarren <ctze.darren@gmail.com>
2024-04-06 15:41:58 +0800

    added all the xml pages for createtourlisting screen

--- app/src/main/java/com/example/wayfare/Fragment/CreateListing/CreateListingFragment3.java
+++ app/src/main/java/com/example/wayfare/Fragment/CreateListing/CreateListingFragment3.java
@@ -1,6 +1,5 @@
 package com.example.wayfare.Fragment.CreateListing;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,23 +7,15 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.wayfare.Adapters.CategoryAdapter;
-import com.example.wayfare.Models.CategoryItemModel;
 import com.example.wayfare.R;
-import com.example.wayfare.RecyclerViewInterface;
 import com.example.wayfare.Utils.Helper;
 
-import java.util.Arrays;
-import java.util.List;
-
-public class CreateListingFragment2 extends Fragment {
+public class CreateListingFragment3 extends Fragment {
 
     Button continue_button;
 
-    public CreateListingFragment2() {
+    public CreateListingFragment3() {
     }
 
     @Override
@@ -36,14 +27,14 @@ public class CreateListingFragment2 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_listing_create2, container, false);
+        View view = inflater.inflate(R.layout.fragment_listing_create3, container, false);
         continue_button = view.findViewById(R.id.continue_button);
 
         continue_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //TODO change to createlisting fragment3
-                Helper.goToFragmentSlideInRight(getParentFragmentManager(), R.id.container, new CreateListingFragment3());
+                Helper.goToFragmentSlideInRight(getParentFragmentManager(), R.id.container, new CreateListingFragment4());
             }
         });
         return view;
