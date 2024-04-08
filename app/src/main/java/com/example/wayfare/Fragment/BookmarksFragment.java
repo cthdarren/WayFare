bbf7995edba921c295de288593cef2795f2c5c44
Gabryel <gabryelsoh@gmail.com>
2024-04-08 12:23:36 +0800

    completed TODO bookmarks

--- app/src/main/java/com/example/wayfare/Fragment/BookmarksFragment.java
+++ app/src/main/java/com/example/wayfare/Fragment/BookmarksFragment.java
@@ -18,6 +18,7 @@ import com.example.wayfare.Adapters.BookmarkAdapter;
 import com.example.wayfare.Models.BookmarkItemModel;
 import com.example.wayfare.Models.BookmarkModel;
 import com.example.wayfare.Models.ResponseModel;
+import com.example.wayfare.Models.TourListModel;
 import com.example.wayfare.R;
 import com.example.wayfare.RecyclerViewInterface;
 import com.example.wayfare.Utils.AuthService;
@@ -98,8 +99,43 @@ public class BookmarksFragment extends Fragment implements RecyclerViewInterface
     @Override
     public void onItemClick(int position) {
         //TODO GO TO LISTING
-//        TourListingFull tourListingFullFragment = new TourListingFull();
-//        tourListingFullFragment.setArguments(data);
-//        Helper.goToFragmentSlideInRight(getParentFragmentManager(), R.id.container, new TourListingFull());
+        String listingId = bookmarkItemModels.get(position).listingId;
+
+        // Making API call for specificc listing
+        new AuthService(getContext()).getResponse("/api/v1/listing/" + listingId, true, Helper.RequestType.REQ_GET, null, new AuthService.ResponseListener() {
+            @Override
+            public void onResponse(ResponseModel json) {
+                if (json.success) {
+                    TourListModel listingDetails = new Gson().fromJson(json.data.getAsJsonObject(), TourListModel.class);
+                    navigateToFullPageFragment(listingDetails);
+                }
+            }
+            @Override
+            public void onError(String message) {
+                // Handle network errorrs
+            }
+        });
+    }
+
+    private void navigateToFullPageFragment(TourListModel listingDetails) {
+        Bundle data = new Bundle();
+        data.putString("title", listingDetails.getTitle());
+        data.putString("location", listingDetails.getRegion());
+        data.putString("rating", String.valueOf(listingDetails.getRating()));
+        data.putString("price", String.valueOf(listingDetails.getPrice()));
+        data.putString("thumbnail", listingDetails.getThumbnailUrls()[0]);
+        data.putString("description", listingDetails.getDescription());
+        data.putString("reviewCount", String.valueOf(listingDetails.getReviewCount()));
+        data.putString("listingId", listingDetails.getId());
+        data.putInt("minPax", listingDetails.getMinPax());
+        data.putInt("maxPax", listingDetails.getMaxPax());
+        data.putString("userId", listingDetails.getUserId());
+        data.putString("category", listingDetails.getCategory());
+        data.putParcelableArrayList("timeRangeList", new ArrayList<>(listingDetails.getTimeRangeList()));
+
+        TourListingFull tourListingFullFragment = new TourListingFull();
+        tourListingFullFragment.setArguments(data);
+
+        Helper.goToFragmentSlideInRightArgs(data, getParentFragmentManager(), R.id.container, tourListingFullFragment);
     }
 }
