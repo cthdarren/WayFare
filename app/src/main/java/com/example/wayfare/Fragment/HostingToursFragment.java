1a180c2d3f6edd8302ad3b8a1cbfa91a0af7ed11
cthdarren <cthdarren88@gmail.com>
2024-04-08 23:27:48 +0800

    added your listings page

--- app/src/main/java/com/example/wayfare/Fragment/HostingToursFragment.java
+++ app/src/main/java/com/example/wayfare/Fragment/HostingToursFragment.java
@@ -1,32 +1,109 @@
 package com.example.wayfare.Fragment;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.wayfare.Adapters.YourListingsAdapter;
+import com.example.wayfare.Adapters.tourListing_RecyclerViewAdapter;
 import com.example.wayfare.Fragment.CreateListing.CreateListingFragment;
+import com.example.wayfare.Models.ResponseModel;
+import com.example.wayfare.Models.TourListModel;
 import com.example.wayfare.R;
+import com.example.wayfare.Utils.AuthService;
 import com.example.wayfare.Utils.Helper;
+import com.example.wayfare.ViewModel.UserViewModel;
+import com.example.wayfare.tourListing_RecyclerViewInterface;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 
-public class HostingToursFragment extends Fragment {
+import java.util.ArrayList;
+
+public class HostingToursFragment extends Fragment  implements tourListing_RecyclerViewInterface {
+    RecyclerView yourListingRecycler;
+    UserViewModel userViewModel;
+    ProgressBar progressBar;
+    ArrayList<TourListModel> tourListModels = new ArrayList<>();
 
     public HostingToursFragment(){}
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        userViewModel = new ViewModelProvider(getActivity()).get(UserViewModel.class);
         View view = inflater.inflate(R.layout.hosting_fragment_tours, container, false);
         ImageView createListingBtn = view.findViewById(R.id.createListingButton);
+        progressBar = view.findViewById(R.id.yourListingsProgBar);
+
+        yourListingRecycler = view.findViewById(R.id.yourListingsRecycler);
+        yourListingRecycler.setAdapter(new YourListingsAdapter(getContext(),tourListModels, this));
+        yourListingRecycler.setLayoutManager(new LinearLayoutManager(getContext()));
+        setupTourListModels();
+
         createListingBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Helper.goToFragmentSlideInRight(getParentFragmentManager(), R.id.container, new CreateListingFragment());
             }
         });
+
+
         return view;
+    }
+    public void setupTourListModels(){
+        new AuthService(getContext()).getResponse("/api/v1/user/listing/" + userViewModel.getUserProfileData().getUsername(), false, Helper.RequestType.REQ_GET, null, new AuthService.ResponseListener() {
+            @Override
+            public void onError(String message) {
+                makeToast(message);
+            }
+
+            @Override
+            public void onResponse(ResponseModel json) {
+                if (json.success){
+                    JsonArray listingArray = json.data.getAsJsonArray();
+                    for (JsonElement je: listingArray){
+                        TourListModel toAdd = new Gson().fromJson(je, TourListModel.class);
+                        tourListModels.add(toAdd);
+                    }
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            yourListingRecycler.getAdapter().notifyDataSetChanged();
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    });
+                }
+            }
+        });
+    }
+    public void makeToast(String msg) {
+
+        if (getActivity() == null) {
+            Log.d("ERROR", "ACTIVITY CONTEXT IS NULL, UNABLE TO MAKE TOAST");
+            return;
+        }
+        getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(getActivity().getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+                progressBar.setVisibility(View.GONE);
+            }
+        });
+    }
+
+    @Override
+    public void onItemClick(int position) {
+
     }
 }
