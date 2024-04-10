51a76c2beb6ad009b551a32d943c244b4787536b
cthdarren <cthdarren88@gmail.com>
2024-04-10 19:35:23 +0800

    minor ui change in upcoming to see if you have additinal bookings

---
+++ app/src/main/java/com/example/wayfare/Fragment/ViewBookingFragment.java
@@ -0,0 +1,21 @@
+package com.example.wayfare.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.wayfare.R;
+
+public class ViewBookingFragment extends Fragment {
+
+    public ViewBookingFragment(){}
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_account, container, false);
+    }
+}
